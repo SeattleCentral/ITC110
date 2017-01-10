8e7750bbfc0bcddb5326c79436d46e9efd44507b
Josh <josh@halfnibble.com>
2017-01-09 18:54:57 -0800

    Add example assignment.

---
+++ assignments/example_assignment.py
@@ -0,0 +1,19 @@
+# Book version 3
+
+# 1.
+print ("Hello, World!")
+# Hello, World!
+print ("Hello", "World!")
+# "Hello World!"
+# ...
+
+
+# 4.
+def main():
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(20):
+        x = 3.9 * x * (1 -x)
+        print(x)
+
+main()
---
+++ examples/lecture02.py
@@ -0,0 +1,10 @@
+# A simple program demonstrating chaotic behavior
+
+def main():
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * x * (1 -x)
+        print(x)
+
+main()
