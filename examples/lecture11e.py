d6880eafa962cdc3e4436f71837ec5f9cf622845
Josh <josh@halfnibble.com>
2017-02-22 20:24:33 -0800

    Updated lecture examples.

--- examples/lecture11e.py
+++ examples/lecture11e.py
@@ -1,4 +1,4 @@
-def move(x, dx, y, dy):
+def move(x=0, dx=10, y=0, dy=10):
     x = x + dx
     y = y + dy
     return x, y
@@ -7,5 +7,8 @@ x = 50
 y = 50
 
 new_x, new_y = move(x, 50, y, -25)
-
 print("The new coords are: ", new_x, ",", new_y)
+
+print("Call with missing x and y...")
+missing_x, missing_y = move(dx=50, dy=50)
+print("The new coords are: ", missing_x, ",", missing_y)
---
+++ examples/lecture12a.py
@@ -0,0 +1,18 @@
+# Python passes by value, but watch out for Lists!
+
+bank_accounts = ["a", "b", "c", "d"]
+
+# `accounts` is a List type []
+def grow_money(accounts, money_to_add):
+    for i in range(len(accounts)):
+        accounts[i] = accounts[i] + money_to_add
+    # Notice: no return statment
+
+return_grow_money = grow_money(bank_accounts, 100.00)
+
+print("Return of grow money is: ")
+print(return_grow_money) # Prints None
+
+print("The new bank accounts are: ")
+print(bank_accounts)     # Prints a modified List!!
+
